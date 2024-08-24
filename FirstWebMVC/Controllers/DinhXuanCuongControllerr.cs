fceca515340c323b3067e47b43f4c333870bf14d
dinhcuong29 <cuonga521dqh@gmail.com>
2024-08-24 16:07:25 +0700

    FirstWebMVC

---
+++ FirstWebMVC/Controllers/DinhXuanCuongControllerr.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstWebMVC.Models;
+
+namespace FirstWebMVC.Controllers;
+public class DinhxuanCuongController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+}
---
+++ FirstWebMVC/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstWebMVC.Models;
+
+namespace FirstWebMVC.Controllers;
+
+public class StudentController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+ 
+}
